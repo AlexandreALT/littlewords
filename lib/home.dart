7957f917f7b641ee8857c30e2423d318109b7d7f
AlexandreALT <alex.ant2510@gmail.com>
2022-12-14 16:51:02 +0100

    Rotation bonhomme

--- lib/home.dart
+++ lib/home.dart
@@ -7,9 +7,7 @@ import 'package:littlewords/word_dto.dart';
 import 'package:location/location.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage
-
-  ({super.key});
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -26,72 +24,80 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Stack(
         children: [
-        FlutterMap(
+          FlutterMap(
+            mapController: mapController,
+            options: MapOptions(
+                zoom: 9.2,
+                onMapReady: () async {
+                  Location location = Location();
 
-        mapController: mapController,
-        options: MapOptions(
-            zoom: 9.2,
-            onMapReady: () async {
-              Location location = Location();
+                  PermissionStatus? _permissionGranted;
+                  LocationData? locationData;
 
-              PermissionStatus? _permissionGranted;
-              LocationData? locationData;
+                  bool _serviceEnabled = await location.serviceEnabled();
 
-              bool _serviceEnabled = await location.serviceEnabled();
+                  if (!_serviceEnabled) {
+                    _serviceEnabled = await location.requestService();
+                    if (!_serviceEnabled) {
+                      return;
+                    }
+                  }
 
-              if (!_serviceEnabled) {
-                _serviceEnabled = await location.requestService();
-                if (!_serviceEnabled) {
-                  return;
-                }
-              }
+                  _permissionGranted = await location.hasPermission();
+                  if (_permissionGranted == PermissionStatus.denied) {
+                    _permissionGranted = await location.requestPermission();
+                    if (_permissionGranted != PermissionStatus.granted) {
+                      return;
+                    }
+                  }
 
-              _permissionGranted = await location.hasPermission();
-              if (_permissionGranted == PermissionStatus.denied) {
-                _permissionGranted = await location.requestPermission();
-                if (_permissionGranted != PermissionStatus.granted) {
-                  return;
-                }
-              }
+                  locationData = await location.getLocation();
+                  setState(() {
+                    position = LatLng(
+                        locationData!.latitude!, locationData.longitude!);
+                  });
 
-              locationData = await location.getLocation();
-              setState(() {
-                position =
-                    LatLng(locationData!.latitude!, locationData.longitude!);
-              });
-
-              mapController.move(
-                  position!, 18);
-            }),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.app',
-          ),
-        ],
-        nonRotatedChildren: [
-          MarkerLayer(
-            markers: [
-              if (position != null) Marker(rotate: false,
-                  point: position!,
-                  width: 80,
-                  height: 80,
-                  builder: (context) =>
-                      Image.asset('assets/personne.png', height: 50,)),
-              for (var w in words)
-                Marker(
-                  point: LatLng(w.latitude!, w.longitude!),
-                  width: 80,
-                  height: 80,
-                  builder: (context) => FlutterLogo(),
+                  mapController.move(position!, 18);
+                }),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+            ],
+            nonRotatedChildren: [
+              MarkerLayer(
+                markers: [
+                  if (position != null)
+                    Marker(
+                        rotate: false,
+                        point: position!,
+                        width: 80,
+                        height: 80,
+                        builder: (context) => Image.asset(
+                              'assets/personne.png',
+                              height: 50,
+                            )),
+                  for (var w in words)
+                    Marker(
+                      point: LatLng(w.latitude!, w.longitude!),
+                      width: 80,
+                      height: 80,
+                      builder: (context) => FlutterLogo(),
+                    ),
+                ],
+              ),
+              Positioned(
+                right: 8,
+                top: 50,
+                child: IconButton(
+                  icon: Image.asset('assets/location.png'),
+                  iconSize: 50,
+                  onPressed: () {},
                 ),
+              )
             ],
           ),
-          Positioned(right: 8, top: 50, child: IconButton(
-            icon: Image.asset('assets/location.png'),
-            iconSize: 50,
-            onPressed: () {},
-          ),)
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
