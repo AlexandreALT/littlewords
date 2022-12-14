74cde9dc2e58b272b8467c64fc5dfe08da583183
AlexandreALT <alex.ant2510@gmail.com>
2022-12-14 16:35:40 +0100

    Bouton

--- lib/home.dart
+++ lib/home.dart
@@ -22,64 +22,67 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(
-        mapController: mapController,
-        options: MapOptions(
-            zoom: 9.2,
-            onMapReady: () async {
-              Location location = Location();
-
-              PermissionStatus? _permissionGranted;
-              LocationData? locationData;
-
-              bool _serviceEnabled = await location.serviceEnabled();
-
-              if (!_serviceEnabled) {
-                _serviceEnabled = await location.requestService();
-                if (!_serviceEnabled) {
-                  return;
-                }
-              }
-
-              _permissionGranted = await location.hasPermission();
-              if (_permissionGranted == PermissionStatus.denied) {
-                _permissionGranted = await location.requestPermission();
-                if (_permissionGranted != PermissionStatus.granted) {
-                  return;
-                }
-              }
-
-              locationData = await location.getLocation();
-              setState(() {
-                position = LatLng(locationData!.latitude!, locationData.longitude!);
-              });
-
-              mapController.move(
-                  position!, 18);
-            }),
+      body: Stack(
         children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.app',
-          ),
-          MarkerLayer(
-            markers: [
-              if (position != null) Marker(point: position!, width:80, height: 80, builder:  (context) => Image.asset('assets/personne.png', height: 50,)),
-              for (var w in words)
-                Marker(
-                  point: LatLng(w.latitude!, w.longitude!),
-                  width: 80,
-                  height: 80,
-                  builder: (context) => const FlutterLogo(),
-                ),
+          FlutterMap(
+            mapController: mapController,
+            options: MapOptions(
+                zoom: 9.2,
+                onMapReady: () async {
+                  Location location = Location();
+
+                  PermissionStatus? _permissionGranted;
+                  LocationData? locationData;
+
+                  bool _serviceEnabled = await location.serviceEnabled();
+
+                  if (!_serviceEnabled) {
+                    _serviceEnabled = await location.requestService();
+                    if (!_serviceEnabled) {
+                      return;
+                    }
+                  }
+
+                  _permissionGranted = await location.hasPermission();
+                  if (_permissionGranted == PermissionStatus.denied) {
+                    _permissionGranted = await location.requestPermission();
+                    if (_permissionGranted != PermissionStatus.granted) {
+                      return;
+                    }
+                  }
+
+                  locationData = await location.getLocation();
+                  setState(() {
+                    position = LatLng(locationData!.latitude!, locationData.longitude!);
+                  });
+
+                  mapController.move(
+                      position!, 18);
+                }),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+              MarkerLayer(
+                markers: [
+                  if (position != null) Marker(point: position!, width:80, height: 80, builder:  (context) => Image.asset('assets/personne.png', height: 50,)),
+                  for (var w in words)
+                    Marker(
+                      point: LatLng(w.latitude!, w.longitude!),
+                      width: 80,
+                      height: 80,
+                      builder: (context) => const FlutterLogo(),
+                    ),
+                ],
+              ),
             ],
           ),
-          Stack(children: [
-            Positioned(right: 8,top: 64,child: IconButton(
-              icon: Image.asset('assets/location.png'),
-              //iconSize: 50,
-              onPressed: () {},
-            ),)]),
+          Positioned(right: 8,top: 50,child: IconButton(
+            icon: Image.asset('assets/location.png'),
+            iconSize: 50,
+            onPressed: () {},
+          ),)
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
